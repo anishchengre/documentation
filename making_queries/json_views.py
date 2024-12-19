85893596c8eca064b873db14a1735f8291018b3c
Anish Gharti <anishchengre@anishgharti.chhetry.com>
2024-12-19 23:15:39 +0545

    working on transforms of data in json

--- making_queries/json_views.py
+++ making_queries/json_views.py
@@ -1,4 +1,11 @@
 
+from django.db.models.fields.json import KT
+from django.http import HttpResponse
+
+from making_queries.models import Dog
 
 
-# def json_view(request):
+def using_kt(request):
+    dogs = Dog.objects.annotate(first_breed = KT("data__breed__1"), owner_name = KT("data__owner__name")).filter(first_breed__startswith='lhasa', owner_name="Bob")
+    print(dogs)
+    return HttpResponse('success')
--- making_queries/urls.py
+++ making_queries/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from making_queries import views
+from making_queries import views, json_views
 
 
 urlpatterns = [
     # path('blogs/', views.blogs_queryset, name='blogs')
     # path('blogs/', views.filter_referencing_fields, name='blogs')
-    path('blogs/', views.filter_using_outer_ref_and_sub_query, name='blogs')
+    # path('blogs/', views.filter_using_outer_ref_and_sub_query, name='blogs')
+    path('blogs/', json_views.using_kt, name='blogs')
 ]
