5fb40db2ed7b0af0865e20cbf43a9d8f75050a3f
Anish Gharti <anishchengre@anishgharti.chhetry.com>
2024-12-19 22:02:33 +0545

    working on jsonfields

--- making_queries/models.py
+++ making_queries/models.py
@@ -37,3 +37,10 @@ class Entry(models.Model):
 
 
     
+
+class Dog(models.Model):
+    name = models.CharField(max_length=200)
+    data = models.JSONField(null=True)
+
+    def __str__(self):
+        return self.name    
