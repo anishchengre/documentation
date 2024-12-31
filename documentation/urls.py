447334feac8a8310201b66c96c6273df93de222e
Anish Gharti <anishchengre@anishgharti.chhetry.com>
2024-12-31 13:47:57 +0545

    created model for aggegations

--- documentation/urls.py
+++ documentation/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('making_queries.urls'))
+    path('', include('making_queries.urls')),
+    path('', include('aggregation.urls'))
 ]
--- making_queries/json_views.py
+++ making_queries/json_views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from making_queries.models import Dog
 
 
-# def using_kt(request):
-#     dogs = Dog.objects.annotate(first_breed = KT("data__breed__1"), owner_name = KT("data__owner__name")).filter(first_breed__startswith='lhasa', owner_name="Bob")
+def using_kt(request):
+    dogs = Dog.objects.annotate(first_breed = KT("data__breed__1"), owner_name = KT("data__owner__name")).filter(first_breed__startswith='lhasa', owner_name="Bob")
 
-#     return HttpResponse('success')
+    return HttpResponse('success')
